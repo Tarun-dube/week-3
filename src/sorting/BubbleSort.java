b574aab8e9211936ce24325ae6264f4149007f55
Tarun Dubey <tarun2019dubey@gmail.com>
2025-01-31 11:03:00 +0530

    sort

--- src/sorting/BubbleSort.java
+++ src/sorting/BubbleSort.java
@@ -2,29 +2,33 @@ package sorting;
 import java.util.*;
 
 public class BubbleSort {
-    public static void main(String[] args) {
-        int[] marks = {1, 2, 7, 8, 3, 9, 4};
-
-        //swap the elementes
-        int n = marks.length;
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
         Boolean swapped;
 
         for (int i = 0; i < n - 1; i++) {
             swapped = false;
             for (int j = 0; j < n - i - 1; j++) {
-                    if (marks[j] > marks[j + 1]) {
-                        int temp = marks[j];
-                        marks[j] = marks[j + 1];
-                        marks[j + 1] = temp;
-                        swapped = true;
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
 
-                    }
+                }
             }
             if (!swapped) {
                 break;
             }
 
         }
+    }
+    public static void main(String[] args) {
+        int[] marks = {1, 2, 7, 8, 3, 9, 4};
+
+        //swap the elementes
+        bubbleSort(marks);
+
         for(int i = 0; i < marks.length; i++){
             System.out.print(marks[i] + " ");
         }
