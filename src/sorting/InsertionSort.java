8619760f32e59a891552a9ce136abc1093856354
Tarun Dubey <tarun2019dubey@gmail.com>
2025-01-31 11:05:03 +0530

    insertion sort

--- src/sorting/InsertionSort.java
+++ src/sorting/InsertionSort.java
@@ -1,4 +1,29 @@
 package sorting;
 
 public class InsertionSort {
+    public static void insertionSort(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            int key = array[i];
+            int j = i - 1;
+            while (j >= 0 && array[j] > key) {
+                array[j + 1] = array[j];
+                j = j - 1;
+
+            }
+            array[j + 1] = key;
+
+        }
+    }
+    public static void main(String[] args) {
+        int[] empId={2,1,3,4,7,6,5};
+        //sort
+        insertionSort(empId);
+
+        //display array
+        for (int id : empId) {
+            System.out.print(id+" ");
+        }
+
+
+    }
 }
