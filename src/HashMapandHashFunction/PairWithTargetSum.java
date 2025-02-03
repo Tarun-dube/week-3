7cbc619ecc83c0631df888991086ee39cbd6c888
Tarun Dubey <tarun2019dubey@gmail.com>
2025-02-03 10:34:21 +0530

    targetsum

---
+++ src/HashMapandHashFunction/PairWithTargetSum.java
@@ -0,0 +1,28 @@
+package HashMapandHashFunction;
+
+import java.util.*;
+
+public class PairWithTargetSum {
+    public static boolean hasPairWithSum(int[] arr, int target) {
+        Set<Integer> seenNumbers = new HashSet<>();
+
+        for (int num : arr) {
+            int complement = target - num;
+            if (seenNumbers.contains(complement)) {
+                System.out.println("Pair found: (" + num + ", " + complement + ")");
+                return true;
+            }
+            seenNumbers.add(num);
+        }
+
+        System.out.println("No pair found.");
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 15, 3, 7};
+        int target = 17;
+        hasPairWithSum(arr, target);
+    }
+}
+
