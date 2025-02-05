e6244a549e7d1740493b1cfdc79ca8a6ead0f841
Tarun Dubey <tarun2019dubey@gmail.com>
2025-02-05 15:32:18 +0530

    RemoveDuplicates

---
+++ src/main/java/stringbuilder/RemoveDuplicates.java
@@ -0,0 +1,29 @@
+package stringbuilder;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class RemoveDuplicates {
+    public static StringBuilder removeDuplicates(String s) {
+        StringBuilder result = new StringBuilder();
+        Set<Character> set = new HashSet<Character>();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (!set.contains(ch)) {
+                set.add(ch);
+                result.append(ch);
+
+            }
+
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      String s = sc.nextLine();
+      StringBuilder result = removeDuplicates(s);
+      System.out.println(result);
+
+    }
+}
