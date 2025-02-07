0c215f8ba9a035d06e675b9098ea14cbbdf075dd
Tarun Dubey <tarun2019dubey@gmail.com>
2025-02-07 12:42:24 +0530

    tarun add. find first and last occurance

---
+++ src/main/java/BinarySearch/BinarySearchRange.java
@@ -0,0 +1,48 @@
+package BinarySearch;
+
+public class BinarySearchRange {
+    public static int[] searchRange(int[] nums, int target) {
+        int first = findFirst(nums, target);
+        int last = findLast(nums, target);
+        return new int[]{first, last};
+    }
+
+    private static int findFirst(int[] nums, int target) {
+        int left = 0, right = nums.length - 1, first = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                first = mid;
+                right = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return first;
+    }
+
+    private static int findLast(int[] nums, int target) {
+        int left = 0, right = nums.length - 1, last = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                last = mid;
+                left = mid + 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return last;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {5, 7, 7, 8, 8, 10};
+        int target = 8;
+        int[] result = searchRange(nums, target);
+        System.out.println("First Occurrence: " + result[0] + ", Last Occurrence: " + result[1]); // Output: [3, 4]
+    }
+}
