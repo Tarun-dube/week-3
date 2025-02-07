0b325d6398a739ba6eb02148d72a47588fbab9da
Tarun Dubey <tarun2019dubey@gmail.com>
2025-02-07 12:50:06 +0530

    tarun add. linear and binary search

---
+++ src/main/java/challangeProblem/LinearAndBinarySearch.java
@@ -0,0 +1,47 @@
+package challangeProblem;
+
+public class LinearAndBinarySearch {
+    public static int findFirstMissingPositive(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
+                int temp = nums[i];
+                nums[i] = nums[temp - 1];
+                nums[temp - 1] = temp;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+
+        return n + 1;
+    }
+
+    public static int binarySearch(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {3, 4, -1, 1};
+        System.out.println("First Missing Positive: " + findFirstMissingPositive(nums)); // Output: 2
+
+        int[] sortedNums = {1, 2, 3, 4, 5, 6};
+        int target = 4;
+        System.out.println("Index of target: " + binarySearch(sortedNums, target)); // Output: 3
+    }
+}
+
